0483192edc136b00ad20c2a839f282c51a7384a4
leeink <zzxc78@naver.com>
2026-04-24 15:43:09 +0900

    settings

---
+++ 3/app/src/main/java/com/example/wikiapp/Travel.kt
@@ -0,0 +1,19 @@
+package com.example.wikiapp
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.example.wikiapp.ui.theme.LightGreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Travel(){
+
+}
