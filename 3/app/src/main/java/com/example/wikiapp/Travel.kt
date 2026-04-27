a11fa1a77e253de53f2d598b0fad0e40fc2f05e1
leeink <zzxc78@naver.com>
2026-04-27 21:51:33 +0900

    03-5 working

--- 3/app/src/main/java/com/example/wikiapp/Travel.kt
+++ 3/app/src/main/java/com/example/wikiapp/Travel.kt
@@ -1,10 +1,12 @@
 package com.example.wikiapp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,19 +14,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -88,7 +97,8 @@ fun Travel() {
             edgePadding = 16.dp,
             selectedTabIndex = selectedTab,
             modifier = Modifier
-                .padding(paddingValues)) {
+                .padding(paddingValues)
+        ) {
             Tabs.entries.forEachIndexed { index, tab ->
                 Tab(
                     selected = selectedTab == index,
@@ -148,33 +158,14 @@ fun PopularScreen() {
                 start = 24.dp,
                 end = 24.dp
             )
-    ){
+    ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
                 .horizontalScroll(rememberScrollState())
-        ){
+        ) {
             repeat(3) {
-                Box(
-                    modifier = Modifier
-                        .width(315.dp)
-                        .height(235.dp)
-                        .clip(
-                            RoundedCornerShape(20.dp)
-                        )
-                        .background(Color.Blue)
-                ){
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(80.dp)
-                            .clip(RoundedCornerShape(20.dp))
-                            .background(Color(0xFFA0A0A0))
-                            .align(Alignment.BottomCenter),
-                    ){
-
-                    }
-                }
+                BannerComponent()
             }
         }
 
@@ -187,7 +178,7 @@ fun PopularScreen() {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.fillMaxWidth()
-        ){
+        ) {
             Text(
                 text = "Recommended",
                 style = MaterialTheme.typography.titleMedium,
@@ -197,10 +188,107 @@ fun PopularScreen() {
                 text = "View All",
                 style = MaterialTheme.typography.titleMedium,
                 maxLines = 1,
+                modifier = Modifier
+                    .clickable(
+                        onClick = {}
+                    )
             )
         }
 
         Spacer(Modifier.height(16.dp))
+
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            maxItemsInEachRow = 2,
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            repeat(6) {
+                BannerComponent(false)
+            }
+        }
+    }
+}
+
+@Composable
+fun BannerComponent(isLarge: Boolean = true){
+    Box(
+        modifier = Modifier
+            .width(if(isLarge) 315.dp else 170.dp)
+            .height(if(isLarge) 250.dp else 170.dp)
+            .clip(
+                RoundedCornerShape(20.dp)
+            )
+            .graphicsLayer {
+                compositingStrategy = CompositingStrategy.Offscreen
+            }
+            .background(Color(0xFFD0CBFF))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(if(isLarge) 80.dp else 56.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .drawWithContent {
+                    drawRect(
+                        color = Color(0xFFA0A0A0),
+                        blendMode = BlendMode.Multiply
+                    )
+                    drawContent()
+                }
+                .align(Alignment.BottomCenter),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = "Northern Mountain",
+                    style = if(isLarge) MaterialTheme.typography.titleLarge
+                        else MaterialTheme.typography.titleSmall,
+                    maxLines = 1,
+                    color = Color.White
+                )
+                Box(
+                    modifier = Modifier
+                        .size(if(isLarge) 24.dp else 16.dp)
+                        .clip(RoundedCornerShape(50.dp))
+                        .background(Color.White),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        tint = Color.Red,
+                        contentDescription = "Like",
+                        modifier = Modifier.size(if (isLarge) 14.dp else 8.dp)
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Row {
+                    repeat(5) {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            tint = Color.Yellow,
+                            contentDescription = "Star",
+                            modifier = Modifier.size(if(isLarge) 14.dp else 8.dp)
+                        )
+                    }
+                }
+                Text(
+                    text = "4.5",
+                    style = MaterialTheme.typography.bodySmall,
+                    maxLines = 1,
+                    color = Color.White
+                )
+            }
+        }
     }
 }
 
@@ -209,26 +297,30 @@ fun RecentScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-    ){
+    ) {
         Text(text = "Recent Screen")
     }
 }
+
 
 @Composable
 fun MostScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-    ){
+    ) {
         Text(text = "Most Screen")
     }
 }
 
 @Composable
-fun FavoriteScreen() {}
+fun FavoriteScreen() {
+}
 
 @Composable
-fun PickScreen() {}
+fun PickScreen() {
+}
 
 @Composable
-fun DummyScreen() {}
+fun DummyScreen() {
+}
